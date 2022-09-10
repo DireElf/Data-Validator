bf2ad5bd6383cbeab26c6357519c7545ab8e34db
DireElf <3672@mail.ru>
2022-09-10 13:34:03 +0700

    refactored StringSchemaTest

--- src/test/java/hexlet/code/StringSchemaTest.java
+++ src/test/java/hexlet/code/StringSchemaTest.java
@@ -9,6 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class StringSchemaTest {
     private StringSchema schema;
+    private final String testString = "what does the fox say";
+    private final int testLength1 = 4;
+    private final int testLength2 = 50;
 
     @BeforeEach
     void setUp() {
@@ -16,32 +19,65 @@ class StringSchemaTest {
     }
 
     @Test
-    void required() {
+    void withoutRequired() {
         assertThat(schema.isValid("")).isTrue();
+        assertThat(schema.isValid("what does the fox say")).isTrue();
         assertThat(schema.isValid(null)).isTrue();
         assertThat(schema.isValid(1)).isTrue();
-        schema.required();
-        assertThat(schema.isValid("")).isFalse();
-        assertThat(schema.isValid(null)).isFalse();
-        assertThat(schema.isValid(1)).isFalse();
-        assertThat(schema.isValid("what does the fox say")).isTrue();
     }
 
     @Test
-    void contains() {
+    void withRequired() {
+        schema.required();
+        assertThat(schema.isValid("what does the fox say")).isTrue();
+        assertThat(schema.isValid("")).isFalse();
+        assertThat(schema.isValid(null)).isFalse();
+        assertThat(schema.isValid(1)).isFalse();
+    }
+
+    @Test
+    void containsCorrectSubstring() {
+        assertThat(schema.contains("").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains(" ").isValid("what does the fox say")).isTrue();
         assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
         assertThat(schema.contains("what").isValid("what does the fox say")).isTrue();
+    }
+
+    @Test
+    void containsIncorrectSubstring() {
+        assertThat(schema.contains(" what").isValid("what does the fox say")).isFalse();
+        assertThat(schema.contains("say ").isValid("what does the fox say")).isFalse();
         assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
         assertThat(schema.isValid("what does the fox say")).isFalse();
     }
 
     @Test
     void minLength() {
-        final int sampleLength = 4;
-        schema.minLength(sampleLength);
+        assertThat(schema.isValid("wha")).isTrue();
+        schema.minLength(testLength1);
         assertThat(schema.isValid("what")).isTrue();
         assertThat(schema.isValid("what ")).isTrue();
-        assertThat(schema.isValid("h")).isFalse();
+        assertThat(schema.isValid("wha")).isFalse();
+        schema.minLength(testLength1 - 1);
+        assertThat(schema.isValid("wh")).isFalse();
+        assertThat(schema.isValid("wha")).isTrue();
+        assertThat(schema.isValid("what")).isTrue();
+    }
+
+    @Test
+    void mixturesWithCorrectArgs() {
+        assertThat(schema.contains("wh").minLength(testLength1).isValid(testString)).isTrue();
+        assertThat(schema.minLength(testLength1 + 1).contains("what").isValid(testString)).isTrue();
+        assertThat(schema.required().contains("wh").minLength(testLength1 - 1).isValid(testString)).isTrue();
+        assertThat(schema.contains("what ").required().minLength(0).isValid(testString)).isTrue();
+    }
+
+    @Test
+    void mixturesWithIncorrectArgs() {
+        assertThat(schema.contains("what").required().minLength(testLength1).isValid("")).isFalse();
+        assertThat(schema.minLength(testLength1 + 1).contains("what").isValid("what")).isFalse();
+        assertThat(schema.contains("wah").isValid(testString + " ")).isFalse();
+        assertThat(schema.contains("what ").minLength(testLength1).isValid(testString + " ")).isFalse();
     }
 
     @AfterEach
